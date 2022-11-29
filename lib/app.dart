f38d2e2d4b2976c5147805e5f46198d50b5b6e2c
Fathur Nor Alif <fathurnoralif@gmail.com>
2022-11-29 18:46:02 +0800

    refactor(main): remove starting project, change MyApp to FamizerApp and move it to app.dart

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FamizerApp extends StatelessWidget {
+  const FamizerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
