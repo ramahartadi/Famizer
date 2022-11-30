ae7f632ec94f34fa5a3bc955aa5d51a9e6ebef7c
Fathur Nor Alif <fathurnoralif@gmail.com>
2022-11-30 15:55:45 +0800

    style(theme): typography and colors extensions

---
+++ package/theme/lib/theme_extensions.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+extension TypographyUtils on BuildContext {
+  ThemeData get theme => Theme.of(this);
+
+  TextStyle? get displayLarge => theme.textTheme.displayLarge;
+  TextStyle? get displayMedium => theme.textTheme.displayMedium;
+  TextStyle? get displaySmall => theme.textTheme.displaySmall;
+
+  TextStyle? get headlineLarge => theme.textTheme.headlineLarge;
+  TextStyle? get headlineMedium => theme.textTheme.headlineMedium;
+  TextStyle? get headlineSmall => theme.textTheme.headlineSmall;
+
+  TextStyle? get titleLarge => theme.textTheme.titleLarge;
+  TextStyle? get titleMedium => theme.textTheme.titleMedium;
+  TextStyle? get titleSmall => theme.textTheme.titleSmall;
+
+  TextStyle? get bodyLarge => theme.textTheme.bodyLarge;
+  TextStyle? get bodyMedium => theme.textTheme.bodyMedium;
+  TextStyle? get bodySmall => theme.textTheme.bodySmall;
+
+  TextStyle? get labelLarge => theme.textTheme.labelLarge;
+  TextStyle? get labelMedium => theme.textTheme.labelMedium;
+  TextStyle? get labelSmall => theme.textTheme.labelSmall;
+}
+
+extension ColorSchemeUtils on BuildContext {
+  ThemeData get theme => Theme.of(this);
+  ColorScheme get colors => theme.colorScheme;
+}
