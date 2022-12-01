93a2966b3535da81c56c6f1d94f23b3f904613fc
Fathur Nor Alif <fathurnoralif@gmail.com>
2022-12-01 19:59:27 +0800

    feature(family_onboarding): Create a new page

---
+++ package/family_onboarding/lib/presentation/family_creation_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FamilyCreationPage extends StatefulWidget {
+  const FamilyCreationPage({super.key});
+
+  @override
+  State<FamilyCreationPage> createState() => _FamilyCreationPageState();
+}
+
+class _FamilyCreationPageState extends State<FamilyCreationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
