6d9d586b0ec68c20ff0441b8f7e477a6ef4d6fb8
Fathur Nor Alif <fathurnoralif@gmail.com>
2022-12-03 20:36:27 +0800

    feature(presentation): signup validation and control

--- package/signup/lib/presentation/signup_page.dart
+++ package/signup/lib/presentation/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:theme/theme.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -70,105 +71,137 @@ class _SignupPageState extends State<SignupPage> {
             Expanded(
               child: Form(
                 key: formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Sign up',
-                      style: context.displaySmall?.copyWith(
-                        color: context.colors.primary,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      controller: emailController,
-                      focusNode: emailFocusNode,
-                      textInputAction: TextInputAction.next,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        label: const Text('Email'),
-                        suffixIcon: IconButton(
-                          focusNode: suffixIconFocusNode,
-                          icon: Icon(
-                            Icons.cancel_outlined,
-                            color: context.colors.onSurfaceVariant,
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'Sign up',
+                          style: context.displaySmall?.copyWith(
+                            color: context.colors.primary,
                           ),
-                          tooltip: 'Delete',
-                          onPressed: () {},
                         ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      controller: passwordController,
-                      focusNode: passwordFocusNode,
-                      textInputAction: TextInputAction.next,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        label: const Text('Password'),
-                        suffixIcon: IconButton(
-                          focusNode: suffixIconFocusNode,
-                          icon: Icon(
-                            Icons.visibility,
-                            color: context.colors.onSurfaceVariant,
-                          ),
-                          tooltip: 'Delete',
-                          onPressed: () {},
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      controller: confirmPasswordController,
-                      focusNode: confirmPasswordFocusNode,
-                      textInputAction: TextInputAction.done,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        label: const Text('Konfirmasi Password'),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            Icons.visibility,
-                            color: context.colors.onSurfaceVariant,
-                          ),
-                          tooltip: 'Delete',
-                          onPressed: () {},
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: context.colors.primary,
-                        elevation: 0,
-                      ),
-                      child: Text(
-                        'Sign up',
-                        style: TextStyle(color: context.colors.onPrimary),
-                      ),
-                      onPressed: () {},
-                    ),
-                    const SizedBox(height: 20),
-                    RichText(
-                      text: TextSpan(
-                        text: 'Sudah mempunyai akun?',
-                        style: context.labelMedium?.copyWith(
-                          color: context.colors.onBackground,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: ' Sign in',
-                            style: context.labelMedium?.copyWith(
-                              color: context.colors.primary,
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          controller: emailController,
+                          focusNode: emailFocusNode,
+                          textInputAction: TextInputAction.next,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            label: const Text('Email'),
+                            suffixIcon: IconButton(
+                              focusNode: suffixIconFocusNode,
+                              icon: Icon(
+                                Icons.cancel_outlined,
+                                color: context.colors.onSurfaceVariant,
+                              ),
+                              tooltip: 'Hapus',
+                              onPressed: () {
+                                emailController.clear();
+                              },
                             ),
-                          )
-                        ],
-                      ),
-                      textAlign: TextAlign.center,
+                          ),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: FormBuilderValidators.email(
+                              errorText: 'Masukan Email yang benar.'),
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          controller: passwordController,
+                          focusNode: passwordFocusNode,
+                          textInputAction: TextInputAction.next,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            label: const Text('Password'),
+                            suffixIcon: IconButton(
+                              focusNode: suffixIconFocusNode,
+                              icon: Icon(
+                                obscurePassword
+                                    ? Icons.visibility_outlined
+                                    : Icons.visibility_off_outlined,
+                                color: context.colors.onSurfaceVariant,
+                              ),
+                              tooltip: 'Lihat',
+                              onPressed: () {
+                                setState(() {
+                                  obscurePassword = !obscurePassword;
+                                });
+                              },
+                            ),
+                          ),
+                          obscureText: obscurePassword,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: FormBuilderValidators.minLength(
+                            6,
+                            errorText: 'Password harus lebih dari 6 karakter.',
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          controller: confirmPasswordController,
+                          focusNode: confirmPasswordFocusNode,
+                          textInputAction: TextInputAction.done,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            label: const Text('Konfirmasi Password'),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                obscureConfirmPassword
+                                    ? Icons.visibility_outlined
+                                    : Icons.visibility_off_outlined,
+                                color: context.colors.onSurfaceVariant,
+                              ),
+                              tooltip: 'Delete',
+                              onPressed: () {
+                                setState(() {
+                                  obscureConfirmPassword =
+                                      !obscureConfirmPassword;
+                                });
+                              },
+                            ),
+                          ),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: FormBuilderValidators.equal(
+                            passwordController.text,
+                            errorText: 'Password tidak sama.',
+                          ),
+                          obscureText: obscureConfirmPassword,
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: context.colors.primary,
+                            elevation: 0,
+                          ),
+                          child: Text(
+                            'Sign up',
+                            style: TextStyle(color: context.colors.onPrimary),
+                          ),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(height: 20),
+                        RichText(
+                          text: TextSpan(
+                            text: 'Sudah mempunyai akun?',
+                            style: context.labelMedium?.copyWith(
+                              color: context.colors.onBackground,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: ' Sign in',
+                                style: context.labelMedium?.copyWith(
+                                  color: context.colors.primary,
+                                ),
+                              )
+                            ],
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
