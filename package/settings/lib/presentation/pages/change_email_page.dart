5246162d4112f3126323ef75e563a855c1e39b2a
Fathur Nor Alif <fathurnoralif@gmail.com>
2022-12-03 23:39:41 +0800

    feature(settings): add controller, validations, suffix icon functionalites

--- package/settings/lib/presentation/pages/change_email_page.dart
+++ package/settings/lib/presentation/pages/change_email_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:theme/theme.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 class ChangeEmailPage extends StatefulWidget {
   const ChangeEmailPage({super.key});
@@ -9,6 +10,21 @@ class ChangeEmailPage extends StatefulWidget {
 }
 
 class _ChangeEmailPageState extends State<ChangeEmailPage> {
+  late TextEditingController emailController;
+  late GlobalKey<FormState> formKey;
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+    formKey = GlobalKey<FormState>();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,17 +42,21 @@ class _ChangeEmailPageState extends State<ChangeEmailPage> {
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-        child: TextFormField(
-          textInputAction: TextInputAction.done,
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            label: Text('Email'),
+        child: Form(
+          key: formKey,
+          child: TextFormField(
+            controller: emailController,
+            textInputAction: TextInputAction.done,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              label: Text('Email'),
+            ),
+            obscureText: true,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: FormBuilderValidators.email(
+              errorText: 'Masukan Email yang benar.',
+            ),
           ),
-          obscureText: true,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          validator: (string) {
-            return null;
-          },
         ),
       ),
     );
--- package/settings/lib/presentation/pages/change_password_page.dart
+++ package/settings/lib/presentation/pages/change_password_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:theme/theme.dart';
 
 class ChangePasswordPage extends StatefulWidget {
@@ -9,6 +10,50 @@ class ChangePasswordPage extends StatefulWidget {
 }
 
 class ChangePasswordPageState extends State<ChangePasswordPage> {
+  late GlobalKey<FormState> formKey;
+
+  late TextEditingController oldPasswordController;
+  late TextEditingController newPasswordController;
+  late TextEditingController confirmNewPasswordController;
+
+  late FocusNode oldPasswordFocusNode;
+  late FocusNode newPasswordFocusNode;
+  late FocusNode confirmNewPasswordFocusNode;
+  late FocusNode suffixIconFocusNode;
+
+  bool obscureOldPassword = true;
+  bool obscureNewPassword = true;
+  bool obscureConfirmPassword = true;
+
+  @override
+  void initState() {
+    super.initState();
+    formKey = GlobalKey<FormState>();
+
+    oldPasswordController = TextEditingController();
+    newPasswordController = TextEditingController();
+    confirmNewPasswordController = TextEditingController();
+
+    oldPasswordFocusNode = FocusNode();
+    newPasswordFocusNode = FocusNode();
+    confirmNewPasswordFocusNode = FocusNode();
+    suffixIconFocusNode = FocusNode(skipTraversal: true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    oldPasswordController.dispose();
+    newPasswordController.dispose();
+    confirmNewPasswordController.dispose();
+
+    oldPasswordFocusNode.dispose();
+    newPasswordFocusNode.dispose();
+    confirmNewPasswordFocusNode.dispose();
+
+    suffixIconFocusNode.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,64 +72,96 @@ class ChangePasswordPageState extends State<ChangePasswordPage> {
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 16),
         child: Form(
+          key: formKey,
           child: Column(
             children: [
               const SizedBox(height: 20),
               TextFormField(
+                controller: oldPasswordController,
+                focusNode: oldPasswordFocusNode,
                 textInputAction: TextInputAction.next,
                 decoration: InputDecoration(
                   border: const OutlineInputBorder(),
                   label: const Text('Password Lama'),
-                  helperText: null,
                   suffixIcon: IconButton(
-                    icon: const Icon(Icons.visibility_outlined),
+                    focusNode: suffixIconFocusNode,
+                    icon: Icon(
+                      obscureOldPassword
+                          ? Icons.visibility_outlined
+                          : Icons.visibility_off_outlined,
+                      color: context.colors.onSurfaceVariant,
+                    ),
                     tooltip: 'Lihat',
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        obscureOldPassword = !obscureOldPassword;
+                      });
+                    },
                   ),
                 ),
-                obscureText: true,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (string) {
-                  return null;
-                },
+                obscureText: obscureOldPassword,
               ),
               const SizedBox(height: 20),
               TextFormField(
+                controller: newPasswordController,
+                focusNode: newPasswordFocusNode,
                 textInputAction: TextInputAction.next,
                 decoration: InputDecoration(
                   border: const OutlineInputBorder(),
                   label: const Text('Password Baru'),
-                  helperText: null,
                   suffixIcon: IconButton(
-                    icon: const Icon(Icons.visibility_outlined),
+                    focusNode: suffixIconFocusNode,
+                    icon: Icon(
+                      obscureNewPassword
+                          ? Icons.visibility_outlined
+                          : Icons.visibility_off_outlined,
+                      color: context.colors.onSurfaceVariant,
+                    ),
                     tooltip: 'Lihat',
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        obscureNewPassword = !obscureNewPassword;
+                      });
+                    },
                   ),
                 ),
-                obscureText: true,
+                obscureText: obscureNewPassword,
                 autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (string) {
-                  return null;
-                },
+                validator: FormBuilderValidators.minLength(
+                  6,
+                  errorText: 'Password harus lebih dari 6 karakter.',
+                ),
               ),
               const SizedBox(height: 20),
               TextFormField(
-                textInputAction: TextInputAction.next,
+                controller: confirmNewPasswordController,
+                focusNode: confirmNewPasswordFocusNode,
+                textInputAction: TextInputAction.done,
                 decoration: InputDecoration(
                   border: const OutlineInputBorder(),
                   label: const Text('Konfirmasi Password Baru'),
-                  helperText: null,
                   suffixIcon: IconButton(
-                    icon: const Icon(Icons.visibility_outlined),
+                    focusNode: suffixIconFocusNode,
+                    icon: Icon(
+                      obscureConfirmPassword
+                          ? Icons.visibility_outlined
+                          : Icons.visibility_off_outlined,
+                      color: context.colors.onSurfaceVariant,
+                    ),
                     tooltip: 'Lihat',
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        obscureConfirmPassword = !obscureConfirmPassword;
+                      });
+                    },
                   ),
                 ),
-                obscureText: true,
+                obscureText: obscureConfirmPassword,
                 autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (string) {
-                  return null;
-                },
+                validator: FormBuilderValidators.equal(
+                  newPasswordController.text,
+                  errorText: 'Password tidak sama.',
+                ),
               ),
             ],
           ),
