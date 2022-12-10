26a609b68148f46d58024798b46acb11ffe1a80f
Fathur Nor Alif <fathurnoralif@gmail.com>
2022-12-10 12:56:36 +0800

    feature(auth, main): add auth package, add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:famizer/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'bloc_observer.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = AppBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
